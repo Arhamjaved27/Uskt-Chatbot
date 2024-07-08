42d41b0f8dcd74d45d045389f2336e1158512724
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-07-08 20:42:53 +0000

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,29 @@ import streamlit.components.v1 as components
 from langchain_core.messages import HumanMessage, AIMessage
 st.set_page_config(page_title="Chatbot", page_icon="🖋")
 
-# def history():
-#     return st.session_state.chat_history
+def history():
+    return st.session_state.chat_history
 
 st.title("Chat-bot Guided Virtual Tour Of Uskt 📖")
-# btn = st.button("Create Knowledgebase")
-# if btn:
-#     create_vector_db()
+btn = st.button("Create Knowledgebase")
+if btn:
+    # create_vector_db()
+    with st.chat_message("AI"):
+            st.markdown("Welcome to university Chatbot, How can i help you .. ?")
 
 
-# if "chat_history" not in st.session_state:
-#     st.session_state.chat_history = []
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
 # #conversation
 
-# for message in st.session_state.chat_history:
-#     if isinstance(message, HumanMessage):
-#         with st.chat_message("Human"):
-#             st.markdown(message.content)
-#     else:    
-#         with st.chat_message("AI"):
-#             st.markdown(message.content)
+for message in st.session_state.chat_history:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+    else:    
+        with st.chat_message("AI"):
+            st.markdown(message.content)
             
         
 
