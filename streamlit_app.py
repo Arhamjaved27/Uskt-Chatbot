b036870938d70fddcee6b43ed4896d46676ab816
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-07-08 19:58:51 +0000

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from typing import Literal
 from dataclasses import dataclass
-# from llm_model import get_qa_chain, create_vector_db
+from llm_model import get_qa_chain, create_vector_db
 import streamlit.components.v1 as components
 from langchain_core.messages import HumanMessage, AIMessage
 st.set_page_config(page_title="Chatbot", page_icon="🖋")
@@ -46,23 +46,23 @@ if user_query is not None and user_query !="":
         st.markdown(user_query)
 
     
-    # with st.chat_message("AI"):
-    #     # chain = get_qa_chain()
+    with st.chat_message("AI"):
+        chain = get_qa_chain()
 
-    #     try:
-    #         ai_response = chain(user_query + "?")
-    #         st.markdown(ai_response["result"])
-    #         # print(response["result"])
-    #     except IndexError as e:
-    #         # print(f"An error occurred: {e}")
-    #         # print("The LLM response was empty or malformed.")
-    #         st.markdown("An error occurred: {e}")
-    #     except Exception as e:
-    #         # print(f"An unexpected error occurred: {e}")
-    #         st.markdown(f"An unexpected error occurred: {e}")
+        try:
+            ai_response = chain(user_query + "?")
+            st.markdown(ai_response["result"])
+            # print(response["result"])
+        except IndexError as e:
+            # print(f"An error occurred: {e}")
+            # print("The LLM response was empty or malformed.")
+            st.markdown("An error occurred: {e}")
+        except Exception as e:
+            # print(f"An unexpected error occurred: {e}")
+            st.markdown(f"An unexpected error occurred: {e}")
 
             
-    #     # st.markdown(ai_response)
-    #     #Hi
-    #     #this is to update ai message in chat-history
-    # st.session_state.chat_history.append(AIMessage(ai_response["result"]))
+        # st.markdown(ai_response)
+        #Hi
+        #this is to update ai message in chat-history
+    st.session_state.chat_history.append(AIMessage(ai_response["result"]))
